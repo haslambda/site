341fd145efa52ab82fd5f95526902e48369531d2
Sanggyu Lee <sanggyu523@naver.com>
2019-06-02 01:44:35 +0900

    Do Not import jinja in contest_format

--- judge/contest_format/default.py
+++ judge/contest_format/default.py
@@ -7,13 +7,39 @@ from django.utils.functional import cached_property
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy
+from django.utils.html import avoid_wrapping
 from django.db import connection
 from judge.timezone import from_database_time
 
 from judge.contest_format.base import BaseContestFormat
 from judge.contest_format.registry import register_contest_format
 from judge.utils.timedelta import nice_repr
-from judge.jinja2.filesize import kbdetailformat
+
+
+def bytesdetailformat(bytes):
+    def _format_size(bytes, callback):
+        bytes = float(bytes)
+
+        KB = 1 << 10
+        MB = 1 << 20
+        GB = 1 << 30
+        TB = 1 << 40
+        PB = 1 << 50
+
+        if bytes < KB:
+            return callback('', bytes)
+        elif bytes < MB:
+            return callback('K', bytes / KB)
+        elif bytes < GB:
+            return callback('M', bytes / MB)
+        elif bytes < TB:
+            return callback('G', bytes / GB)
+        elif bytes < PB:
+            return callback('T', bytes / TB)
+        else:
+            return callback('P', bytes / PB)
+
+    return avoid_wrapping(_format_size(bytes * 1024, lambda x, y: ['%d %sB', '%.2f %sB'][bool(x)] % (y, x)))
 
 
 @register_contest_format('default')
@@ -60,7 +86,7 @@ class DefaultContestFormat(BaseContestFormat):
                 url=reverse('contest_user_submissions',
                             args=[self.contest.key, participation.user.user.username, contest_problem.problem.code]),
                 points=floatformat(format_data['points']),
-                codesize=kbdetailformat(format_data['codesize'])
+                codesize=bytesdetailformat(format_data['codesize'])
             )
         else:
             return mark_safe('<td></td>')
@@ -69,7 +95,7 @@ class DefaultContestFormat(BaseContestFormat):
         return format_html(
             u'<td class="user-points">{points}<div class="solving-time">{cumtime}</div></td>',
             points=floatformat(participation.score),
-            cumtime=kbdetailformat(participation.cumsize)
+            cumtime=bytesdetailformat(participation.cumsize)
         )
 
     def get_problem_breakdown(self, participation, contest_problems):
